f30169e04a310d81166dfd058fd674096227ea1f
Junliang Hu <jlhu@cse.cuhk.edu.hk>
2025-08-13 00:42:09 +0800

    demeter: pte_scanner: change to an isolate-scan-putback implementation

--- mm/demeter/pte_scanner.c
+++ mm/demeter/pte_scanner.c
@@ -30,7 +30,8 @@ static struct task_struct *scanner_thread;
 enum {
 	SCAN_INTERVAL_MS_DEFAULT = 100,
 	SCAN_INTERVAL_MS_MIN = 10,
-	SCAN_INTERVAL_MS_MAX = 3600 * 1000
+	SCAN_INTERVAL_MS_MAX = 3600 * 1000,
+	ISOLATE_BATCH_SIZE = 512,
 };
 static unsigned int scan_interval = SCAN_INTERVAL_MS_DEFAULT;
 module_param(scan_interval, uint, 0644);
@@ -66,10 +67,11 @@ static unsigned long scan_lru_list(struct lruvec *lruvec, enum lru_list lru,
 {
 	struct list_head *src = &lruvec->lists[lru];
 	struct page *page, *next;
-	unsigned long pages_scanned = 0, vm_flags = 0;
+	unsigned long pages_scanned = 0, vm_flags = 0, flags, lruvec_flags = 0;
 
 	/* Take the LRU lock for this lruvec */
-	spin_lock_irq(&lruvec->lru_lock);
+	spin_lock_irqsave(&lruvec->lru_lock, flags);
+	// relock_page_lruvec_irqsave(page, lruvec, &lruvec_flags);
 
 	list_for_each_entry_safe (page, next, src, lru) {
 		/* Skip if page is being freed or is compound tail */
@@ -77,19 +79,105 @@ static unsigned long scan_lru_list(struct lruvec *lruvec, enum lru_list lru,
 		    !page_evictable(page))
 			continue;
 
+		get_page(page);
 		/* Use kernel's page_referenced to examine PTE.A bits and trigger TLB flushes */
 		(*referenced) += page_referenced(page, false, memcg, &vm_flags);
 		pages_scanned++;
+		put_page(page);
 
 		// /* Don't scan too many pages at once to avoid long lock hold times */
 		// if (pages_scanned >= 512)
 		// 	break;
 	}
 
-	spin_unlock_irq(&lruvec->lru_lock);
+	spin_unlock_irqrestore(&lruvec->lru_lock, flags);
 
 	return pages_scanned;
 }
+
+extern unsigned long rotate_lru_list(struct lruvec *lruvec, enum lru_list lru,
+				     struct mem_cgroup *memcg,
+				     unsigned long *referenced);
+#if 0
+// This function uses structures privately defined in mm/vmscan.c
+unsigned long rotate_lru_list(struct lruvec *lruvec, enum lru_list lru,
+			      struct mem_cgroup *memcg,
+			      unsigned long *referenced)
+{
+	enum {
+		ISOLATE_BATCH_SIZE = 512,
+	};
+	// isolate_lru_pages uses: reclaim_idx may_unmap order
+	struct scan_control control = {
+		// Fields used by isolate_lru_pages
+		.reclaim_idx = ZONE_NORMAL,
+		.may_unmap = 1,
+		// Use 0 as its the fallback value used by kswapd
+		.order = 0,
+		// Others
+		.target_mem_cgroup = memcg,
+	}, *sc = &control;
+	unsigned long total_scanned = 0;
+
+	for (unsigned long nr_scanned = 0, nr_taken = 0;
+	     !list_empty(&lruvec->lists[lru]);) {
+		LIST_HEAD(page_list);
+		// Isolate
+		{
+			unsigned long flags = 0;
+			spin_lock_irqsave(&lruvec->lru_lock, flags);
+			nr_taken = isolate_lru_pages(ISOLATE_BATCH_SIZE, lruvec,
+						     &page_list, &nr_scanned,
+						     sc, lru);
+			// We behave like kswapd so borrow its stastics PGSCAN_KSWAPD
+			enum vm_event_item item = PGSCAN_KSWAPD;
+			if (!cgroup_reclaim(sc))
+				__count_vm_events(item, nr_scanned);
+			__count_memcg_events(lruvec_memcg(lruvec), item,
+					     nr_scanned);
+			// We only scan anon pages, but try to be safe anyway
+			__count_vm_events(PGSCAN_ANON + is_file_lru(lru),
+					  nr_scanned);
+			spin_unlock_irqrestore(&lruvec->lru_lock, flags);
+		}
+
+		// Scan
+		{
+			struct page *page, *next;
+			list_for_each_entry_safe (page, next, &page_list, lru) {
+				unsigned long vm_flags = 0;
+				// Use kernel's page_referenced to examine PTE.A bits and trigger TLB flushes
+				(*referenced) += page_referenced(
+					page, false, memcg, &vm_flags);
+				++total_scanned;
+				cond_resched();
+			}
+		}
+
+		// Putback
+		{
+			unsigned long flags = 0;
+			spin_lock_irqsave(&lruvec->lru_lock, flags);
+			move_pages_to_lru(lruvec, &page_list);
+			__mod_node_page_state(
+				lruvec_pgdat(lruvec),
+				NR_ISOLATED_ANON + is_file_lru(lru), -nr_taken);
+			// We do not reclaim pages, so we do not need to update PGSTEAL_KSWAPD counters
+			spin_unlock_irqrestore(&lruvec->lru_lock, flags);
+		}
+
+		// We cannot get a full batch of pages, so stop scanning and try again later
+		// || nr_taken != nr_scanned
+		if (nr_scanned < ISOLATE_BATCH_SIZE)
+			break;
+
+		cond_resched();
+	}
+
+	return total_scanned;
+}
+EXPORT_SYMBOL(rotate_lru_list);
+#endif
 
 static unsigned long scan_zone(struct zone *zone, unsigned long *referenced)
 {
@@ -101,8 +189,12 @@ static unsigned long scan_zone(struct zone *zone, unsigned long *referenced)
 			mem_cgroup_lruvec(memcg, zone->zone_pgdat);
 		enum lru_list lru;
 		for_each_evictable_lru (lru) {
+			if (is_file_lru(lru))
+				continue;
+			// pages_scanned +=
+			// 	scan_lru_list(lruvec, lru, memcg, referenced);
 			pages_scanned +=
-				scan_lru_list(lruvec, lru, memcg, referenced);
+				rotate_lru_list(lruvec, lru, memcg, referenced);
 			/* Yield CPU periodically */
 			if (need_resched()) {
 				cond_resched();
--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4725,3 +4725,81 @@ void check_move_unevictable_pages(struct pagevec *pvec)
 	}
 }
 EXPORT_SYMBOL_GPL(check_move_unevictable_pages);
+
+unsigned long rotate_lru_list(struct lruvec *lruvec, enum lru_list lru,
+			      struct mem_cgroup *memcg,
+			      unsigned long *referenced)
+{
+	enum {
+		ISOLATE_BATCH_SIZE = 512,
+	};
+	// isolate_lru_pages uses: reclaim_idx may_unmap order
+	struct scan_control control = {
+		// Fields used by isolate_lru_pages
+		.reclaim_idx = ZONE_NORMAL,
+		.may_unmap = 1,
+		// Use 0 as its the fallback value used by kswapd
+		.order = 0,
+		// Others
+		.target_mem_cgroup = memcg,
+	}, *sc = &control;
+	unsigned long total_scanned = 0;
+
+	for (unsigned long nr_scanned = 0, nr_taken = 0;
+	     !list_empty(&lruvec->lists[lru]);) {
+		LIST_HEAD(page_list);
+		// Isolate
+		{
+			unsigned long flags = 0;
+			spin_lock_irqsave(&lruvec->lru_lock, flags);
+			nr_taken = isolate_lru_pages(ISOLATE_BATCH_SIZE, lruvec,
+						     &page_list, &nr_scanned,
+						     sc, lru);
+			// We behave like kswapd so borrow its stastics PGSCAN_KSWAPD
+			enum vm_event_item item = PGSCAN_KSWAPD;
+			if (!cgroup_reclaim(sc))
+				__count_vm_events(item, nr_scanned);
+			__count_memcg_events(lruvec_memcg(lruvec), item,
+					     nr_scanned);
+			// We only scan anon pages, but try to be safe anyway
+			__count_vm_events(PGSCAN_ANON + is_file_lru(lru),
+					  nr_scanned);
+			spin_unlock_irqrestore(&lruvec->lru_lock, flags);
+		}
+
+		// Scan
+		{
+			struct page *page, *next;
+			list_for_each_entry_safe (page, next, &page_list, lru) {
+				unsigned long vm_flags = 0;
+				// Use kernel's page_referenced to examine PTE.A bits and trigger TLB flushes
+				(*referenced) += page_referenced(
+					page, false, memcg, &vm_flags);
+				++total_scanned;
+				cond_resched();
+			}
+		}
+
+		// Putback
+		{
+			unsigned long flags = 0;
+			spin_lock_irqsave(&lruvec->lru_lock, flags);
+			move_pages_to_lru(lruvec, &page_list);
+			__mod_node_page_state(
+				lruvec_pgdat(lruvec),
+				NR_ISOLATED_ANON + is_file_lru(lru), -nr_taken);
+			// We do not reclaim pages, so we do not need to update PGSTEAL_KSWAPD counters
+			spin_unlock_irqrestore(&lruvec->lru_lock, flags);
+		}
+
+		// We cannot get a full batch of pages, so stop scanning and try again later
+		// || nr_taken != nr_scanned
+		if (nr_scanned < ISOLATE_BATCH_SIZE)
+			break;
+
+		cond_resched();
+	}
+
+	return total_scanned;
+}
+EXPORT_SYMBOL(rotate_lru_list);
